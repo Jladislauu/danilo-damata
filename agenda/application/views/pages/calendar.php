7687f64ec9c7765903dd813dca45ab72c60c06e5
João Ladislau <126190669+Jladislauu@users.noreply.github.com>
2025-07-01 16:07:02 -0300

    com o js ptbr de novo 3

--- agenda/application/views/pages/calendar.php
+++ agenda/application/views/pages/calendar.php
@@ -133,7 +133,7 @@
 <?php section('scripts'); ?>
 
 <script src="<?= asset_url('assets/vendor/fullcalendar/index.global.min.js') ?>"></script>
-<script src="<?= asset_url('assets/vendor/fullcalendar/locales/pt-br.global.js') ?>"></script>
+<script src="<?= asset_url('assets/vendor/fullcalendar/locales/pt-br.global.min.js') ?>"></script>
 <script src="<?= asset_url('assets/vendor/fullcalendar-moment/index.global.min.js') ?>"></script>
 <script src="<?= asset_url('assets/vendor/jquery-jeditable/jquery.jeditable.min.js') ?>"></script>
 <script src="<?= asset_url('assets/js/utils/date.js') ?>"></script>
