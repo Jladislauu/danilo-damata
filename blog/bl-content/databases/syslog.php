c39fa7844072bbdf0b06a4d4fc40e39c12f69a58
João Ladislau <126190669+Jladislauu@users.noreply.github.com>
2025-07-03 11:21:22 -0300

    Adiciona novamente bl-content ao repositório

---
+++ blog/bl-content/databases/syslog.php
@@ -0,0 +1,83 @@
+<?php defined('BLUDIT') or die('Bludit CMS.'); ?>
+[
+    {
+        "date": "2025-06-27 13:53:24",
+        "dictionaryKey": "content-edited",
+        "notes": "Sobre",
+        "idExecution": "685ecc8428288",
+        "method": "POST",
+        "username": "jladislauu"
+    },
+    {
+        "date": "2025-06-27 13:52:36",
+        "dictionaryKey": "user-edited",
+        "notes": "danilodamata",
+        "idExecution": "685ecc54020cb",
+        "method": "POST",
+        "username": "jladislauu"
+    },
+    {
+        "date": "2025-06-27 13:52:16",
+        "dictionaryKey": "plugin-activated",
+        "notes": "Sitemap",
+        "idExecution": "685ecc40427cb",
+        "method": "GET",
+        "username": "jladislauu"
+    },
+    {
+        "date": "2025-06-27 13:47:38",
+        "dictionaryKey": "plugin-configured",
+        "notes": "About",
+        "idExecution": "685ecb2a3e03c",
+        "method": "POST",
+        "username": "jladislauu"
+    },
+    {
+        "date": "2025-06-27 13:47:32",
+        "dictionaryKey": "plugin-configured",
+        "notes": "About",
+        "idExecution": "685ecb24835bc",
+        "method": "POST",
+        "username": "jladislauu"
+    },
+    {
+        "date": "2025-06-27 13:46:33",
+        "dictionaryKey": "content-edited",
+        "notes": "Sobre",
+        "idExecution": "685ecae990b9e",
+        "method": "POST",
+        "username": "jladislauu"
+    },
+    {
+        "date": "2025-06-27 13:46:23",
+        "dictionaryKey": "content-edited",
+        "notes": "Sobre",
+        "idExecution": "685ecadfec380",
+        "method": "POST",
+        "username": "jladislauu"
+    },
+    {
+        "date": "2025-06-27 13:43:57",
+        "dictionaryKey": "plugin-configured",
+        "notes": "RSS Feed",
+        "idExecution": "685eca4d0e882",
+        "method": "POST",
+        "username": "jladislauu"
+    },
+    {
+        "date": "2025-06-27 13:43:51",
+        "dictionaryKey": "plugin-activated",
+        "notes": "RSS Feed",
+        "idExecution": "685eca47f1fe0",
+        "method": "GET",
+        "username": "jladislauu"
+    },
+    {
+        "date": "2025-06-27 11:27:24",
+        "dictionaryKey": "new-category-created",
+        "notes": "Not\u00edcias ",
+        "idExecution": "685eaa4c7a24f",
+        "method": "POST",
+        "username": "admin"
+    }
+]
